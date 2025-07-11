488f351e14cb1d5e0a405e1d3352a70c673e729b
Mac Book Pro <macbookpro@MacBookPro.modem>
2025-07-11 23:26:03 +1000

    Fix: Set full-screen map layout

--- traffic-mvp/src/components/HereMapComponent.jsx
+++ traffic-mvp/src/components/HereMapComponent.jsx
@@ -3,19 +3,26 @@ import 'leaflet/dist/leaflet.css';
 
 export default function HereMapComponent() {
   return (
-    <MapContainer
-      center={[-37.8136, 144.9631]} // Melbourne
-      zoom={13}
-      scrollWheelZoom={true}
-      style={{ height: '100vh', width: '100%' }}
+    <div
+      id="map"
+      style={{
+        width: '100vw',
+        height: '100vh',
+      }}
     >
-      <TileLayer
-        attribution='&copy; OpenStreetMap contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[-37.8136, 144.9631]}>
-        <Popup>Melbourne CBD</Popup>
-      </Marker>
-    </MapContainer>
+      <MapContainer
+        center={[-37.8136, 144.9631]} // Melbourne
+        zoom={13}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; OpenStreetMap contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[-37.8136, 144.9631]}>
+          <Popup>Melbourne CBD</Popup>
+        </Marker>
+      </MapContainer>
+    </div>
   );
 } 
