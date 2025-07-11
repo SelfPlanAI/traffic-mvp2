d04ca35205a01be21103e6284c36d988040e1b4e
Mac Book Pro <macbookpro@MacBookPro.modem>
2025-07-12 00:12:17 +1000

    Update MapComponent with HERE Maps integration using API key

--- traffic-mvp/src/components/MapComponent.jsx
+++ traffic-mvp/src/components/MapComponent.jsx
@@ -1,38 +1,25 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-import L from "leaflet";
+import { MapContainer, TileLayer } from 'react-leaflet';
 
-// Needed if you still want custom icons:
-import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
-import markerIcon from "leaflet/dist/images/marker-icon.png";
-import markerShadow from "leaflet/dist/images/marker-shadow.png";
+const hereApiKey = import.meta.env.VITE_HERE_API_KEY;
 
-// Fix for default icon (if not done in main.jsx)
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: markerIcon2x,
-  iconUrl: markerIcon,
-  shadowUrl: markerShadow,
-});
+const tileLayerUrl = hereApiKey
+  ? `https://{s}.base.maps.ls.hereapi.com/maptile/2.1/maptile/newest/normal.day/{z}/{x}/{y}/256/png8?apikey=${hereApiKey}`
+  : 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 
 export default function MapComponent() {
-  const hereApiKey = import.meta.env.VITE_HERE_API_KEY;
-
-  const tileLayerUrl = hereApiKey
-    ? `https://{s}.base.maps.ls.hereapi.com/maptile/2.1/maptile/newest/normal.day/{z}/{x}/{y}/256/png8?apikey=${hereApiKey}`
-    : "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
-
   return (
-    <MapContainer>
+    <MapContainer
+      center={[-37.8136, 144.9631]}
+      zoom={15}
+      style={{ height: '100vh', width: '100%' }}
+    >
       <TileLayer
-        url={tileLayerUrl}
-        attribution='Map &copy; HERE or OpenStreetMap'
-        subdomains={['1', '2', '3', '4']}
-        tileSize={256}
+        url={`https://{s}.base.maps.ls.hereapi.com/maptile/2.1/maptile/newest/normal.day/{z}/{x}/{y}/256/png8?apikey=F0UC3TiYy2x5ki4eRf3D`}
+        attribution='Map &copy; <a href="https://developer.here.com">HERE</a>'
+        subdomains={["1", "2", "3", "4"]}
+        detectRetina={true}
       />
-      <Marker position={[-37.8136, 144.9631]}>
-        {/* Marker content */}
-      </Marker>
+      {/* other layers/components here */}
     </MapContainer>
   );
 } 
